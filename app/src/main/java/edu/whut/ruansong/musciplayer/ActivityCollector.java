a9a6e7160a860fe5b78df39787a9d9355282afc3
Lvdou-Jack <rs249310509@163.com>
2018-11-17 11:05:56 +0800

    Init the project.

---
+++ app/src/main/java/edu/whut/ruansong/musciplayer/ActivityCollector.java
@@ -0,0 +1,30 @@
+package edu.whut.ruansong.musciplayer;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 阮阮 on 2018/11/17.
+ */
+
+public class ActivityCollector {
+    public static List<Activity> activities = new ArrayList<>();
+
+    public static void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    public static void removeActivity(Activity activity) {
+        activities.remove(activity);
+    }
+
+    public static void finishAll() {
+        for (Activity activity : activities) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+    }
+}
